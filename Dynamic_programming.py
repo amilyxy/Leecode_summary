ef6ccb7543b4c8a4a86896bd6100b5d135e4a494
amilyxy <amilyxy@163.com>
2019-09-10 21:24:20 +0800

    update Dynamic Programming

--- Dynamic_programming.py
+++ Dynamic_programming.py
@@ -38,3 +38,50 @@ class Solution:
             stage1 = stage2
             stage2 = res
         return res
+
+'''
+62. Unique Paths: 不同路径
+describe: 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+          机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+          问总共有多少条不同的路径？
+'''
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        # 超出时间限制
+        if m == 0 or n == 0:
+            return 1
+        def findpath(i, j):
+            if i == (m-1) or j == (n-1):
+                return 1
+            return (findpath(i+1, j)+ findpath(i, j+1))
+        res = findpath(0, 0)
+        return res
+
+    # 方法一 排列组合
+    def uniquePaths(self, m: int, n: int) -> int:
+        def jiec(i, j):
+            res = 1
+            for k in range(j, j-i, -1):
+                res = k*res
+            return res
+        steps = jiec(n-1, m+n-2)//jiec(n-1, n-1)
+        return steps
+
+    # 方法二 题解的动态规划法
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1]*n] + [[1]+[0] * (n-1) for _ in range(m-1)]
+        # print(dp)
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
+    # 官方提供的还有两种优化算法，可以看一下，有点难理解
+
+
+
+
+
+
+
+
+
