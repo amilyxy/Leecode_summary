a2f2f0c3f4f5c4b2c71c8a08ea989ae4db33cf50
amilyxy <amilyxy@163.com>
2019-11-03 11:10:53 +0800

    add 146 LRU

--- Design.py
+++ Design.py
@@ -55,3 +55,122 @@ class PeekingIterator:
         if self.val:
             return True
         return self.cur.hasNext()
+
+'''
+146. LRU Cache LRU缓存机制
+'''
+# 写的非常不友好 我知道 ==|  我不知道python有orderdict 而且 删除可以真的非常复杂
+from collections import deque
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.data = deque()
+        self.key = set()
+
+    def get(self, key: int) -> int:
+        temp = None
+        for i in self.data:
+            if key in i:
+                temp = {key: i[key]}
+                break
+        if temp:
+            self.data.remove(temp)
+            self.data.append(temp)
+            return temp[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        length = len(self.data)
+        indata = {key: value}
+        if key in self.key:
+            for i in self.data:
+                if key in i:
+                    i[key] = value
+                    temp = {key: value}
+                    break
+            self.data.remove(temp)
+            self.data.append(temp)
+        else:
+            if length >= self.cap:
+                a = self.data.popleft()
+                for i in a:
+                    self.key.remove(i)
+            self.key.add(key)
+            self.data.append(indata)
+
+# 官方题解方法
+from collections import OrderedDict
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.data = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self.data:
+            self.data.move_to_end(key)
+            return self.data[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.data:
+            self.data.move_to_end(key)
+            self.data[key] = value
+        else:
+            if len(self.data) >= self.cap:
+                self.data.popitem(last=False)
+            self.data[key] = value
+
+# 还有一个原始实现方法 感觉写的很不错 @Liye
+class ListNode:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashmap = {}
+        # 新建两个节点 head 和 tail
+        self.head = ListNode()
+        self.tail = ListNode()
+        # 初始化链表为 head <-> tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # 因为get与put操作都可能需要将双向链表中的某个节点移到末尾，所以定义一个方法
+    def move_node_to_tail(self, key):
+            node = self.hashmap[key]
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            node.prev = self.tail.prev
+            node.next = self.tail
+            self.tail.prev.next = node
+            self.tail.prev = node
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            self.move_node_to_tail(key)
+        res = self.hashmap.get(key, -1)
+        if res == -1:
+            return res
+        else:
+            return res.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            self.hashmap[key].value = value
+            self.move_node_to_tail(key)
+        else:
+            if len(self.hashmap) == self.capacity:
+                self.hashmap.pop(self.head.next.key)
+                self.head.next = self.head.next.next
+                self.head.next.prev = self.head
+            # 如果不在的话就插入到尾节点前
+            new = ListNode(key, value)
+            self.hashmap[key] = new
+            new.prev = self.tail.prev
+            new.next = self.tail
+            self.tail.prev.next = new
+            self.tail.prev = new
