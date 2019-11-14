6710b817d55a20bc8f4f2fd18d2becdded4fa07e
amilyxy <amilyxy@163.com>
2019-11-14 20:54:31 +0800

    math add 258 8 67

--- Math.py
+++ Math.py
@@ -107,3 +107,91 @@ class Solution:
             else:
                 break
         return digits
+
+'''
+8. String to Integer (atoi) 字符串转换整数
+'''
+# 感觉真的是笨方法hhhh
+class Solution:
+    def myAtoi(self, str: str) -> int:
+        res, i, neg = 0, 0, 0
+        strin = str.lstrip()
+        n = len(strin)
+        nums = set(['0','1','2','3','4','5','6','7','8','9'])
+        if n == 0:
+            return res
+        if strin[0] == '-' or strin[0] == '+':
+            i = 1
+            if strin[0] == '-':
+                neg = 1
+        while i<n and strin[i] in nums:
+            res = res*10 + int(strin[i])
+            i+=1
+        if neg:
+            res = -res
+        if res>pow(2,31)-1:
+            res = pow(2,31)-1
+        if res<-pow(2, 31):
+            res = -pow(2, 31)
+        return res
+
+# 看到题目的第一眼我就觉得可以用正则，但是我不会用py的正则，答案肯定有人用呀
+import re
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return max(min(int(*re.findall('^[\+\-]?\d+', s.lstrip())), 2**31 - 1), -2**31)
+
+'''
+258. Add Digits 各位相加
+好吧，看答案大家好像都是用%9去做的 == 是我naive
+'''
+class Solution:
+    def addDigits(self, num: int) -> int:
+        res = 0
+        while num:
+            res += num%10
+            num //=10
+            if not num and res>=10:
+                num = res
+                res = 0
+        return res
+# 方法二： 巧了嘿！ 昨天正好学习了eval() 今天就能用上
+    def addDigits(self, num: int) -> int:
+        while len(str(num))>1:
+            num = eval('+'.join(list(str(num))))
+        return num
+# 题解方法三  %9
+    def addDigits(self, num: int) -> int:
+        if num > 9:
+            num = num % 9
+            if num == 0:
+                return 9
+        return num
+
+'''
+67. Add Binary 二进制求和
+题意还是按原始的方法做吧！ 感觉方法还是蛮多的比如异或 不写了~
+'''
+class Solution:
+# 真的是最原始的加减法hhh
+    def addBinary(self, a: str, b: str) -> str:
+        na = len(a)
+        nb = len(b)
+        if na > nb:
+            b = '0'*(na-nb) + b
+        else:
+            a = '0'*(nb-na) + a
+        add = 0
+        n =len(a)
+        res = ''
+        for i in range(n-1, -1, -1):
+            temp = int(a[i]) + int(b[i]) + add
+            res = str(temp%2) + res
+            add = 0 if temp<2 else 1
+        if add == 1:
+            res = '1' + res
+        return res
+# 方法二： python的骚操作
+    def addBinary(self, a: str, b: str) -> str:
+        res = bin(int(a, 2) + int(b, 2))
+        return res[2:]
